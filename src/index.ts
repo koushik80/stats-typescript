1673bde9ff0e233bddef5cb33312fe275e0d50d7
Koushik Dey <koushik.dey@live.com>
2023-02-23 23:02:13 +0200

    extracting csv with a reusable class reader

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,20 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-// To read file
-const matches = fs.readFileSync('football.csv', {
-  encoding: 'utf8'
-})
-  .split('\n')
-  .map(
-    (row: string): string[] => {
-      return row.split(',');
-    }
-);
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
 // H, A, D are from csv file
 // enum - enumeration
 
-enum MatchResult {
+export enum MatchResult {
   HomeWin = 'H',
   AwayWin = 'A',
   Draw = 'D'
 };
 
-const printMatchResult = (): MatchResult => {
-  if (match[5] === 'H') {
-    return MatchResult.HomeWin;
-  }
-
-  return MatchResult.AwayWin;
-};
-
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
