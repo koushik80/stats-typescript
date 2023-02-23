c970be7df29efece83b92497a43d76798dba4fc1
Koushik Dey <koushik.dey@live.com>
2023-02-23 21:38:20 +0200

    reading csv file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello TS');
+import fs from 'fs';
+
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf8'
+})
+  .split('\n')
+  .map(
+    (row: string): string[] => {
+      return row.split(',');
+    }
+  );
+
+console.log(matches);
