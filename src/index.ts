22b6cca952432ccc5234c8a4d1f3a1ddb683a33c
Koushik Dey <koushik.dey@live.com>
2023-02-24 16:54:40 +0200

    generated html report

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { CsvFileReader } from './CsvFileReader';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 // Create an object that satisfies 'DataReader' interface
 
@@ -16,7 +17,7 @@ matchReader.load();
 
 const summary = new Summary(
   new WinsAnalysis('Man United'),
-  new ConsoleReport()
+  new HtmlReport()
 );
 
 summary.buildAndPrintReport(matchReader.matches);
