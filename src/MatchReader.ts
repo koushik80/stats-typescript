a4c2947c0ac88410fe232b21a75d7356063881ee
Koushik Dey <koushik.dey@live.com>
2023-02-24 17:23:50 +0200

    static method

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,14 +1,18 @@
 import { dateStringToDate } from './utils';
 import { MatchResult } from './MatchResult';
 import { MatchData } from './MatchData';
+import { CsvFileReader } from './CsvFileReader';
 
 interface DataReader {
   read(): void;
   data: string[][];
 }
 export class MatchReader {
-  matches: MatchData[] = [];
+  static fromCsv(filename: string): MatchReader {
+    return new MatchReader(new CsvFileReader(filename));
+  }
 
+  matches: MatchData[] = [];
 
   constructor(public reader: DataReader) { }
 
